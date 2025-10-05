345adff516df8adba9e7e2b2d2287f25c079da8b
Ivan <kireev.ivan.8@gmail.com>
2025-10-05 15:00:14 +0700

    feat: js

---
+++ layout/test.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Test JavaScript loaded');
+    const h1 = document.querySelector('h1');
+    if (h1) {
+        h1.addEventListener('click', function() {
+            alert('Hello from JavaScript!');
+        });
+    }
+});
